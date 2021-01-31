5d6e5f1e60007248a6367706c594a6803fefd6dc
ShubhamPatilsd <55328098+ShubhamPatilsd@users.noreply.github.com>
2021-01-31 12:46:15 -0800

    another test halp plz

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-require('dotenv').config();
+var requirejs = require('requirejs');
+
+requirejs.config({
+    //Pass the top-level main.js/index.js require
+    //function to requirejs so that node modules
+    //are loaded relative to the top-level JS file.
+    nodeRequire: require
+});
+
+requirejs('dotenv').config();
 console.log(process.env.TEST);
 
 const firebaseConfig = {
